6bc597c18399d64781a845c4f676c1ce2516c81b
Marko Kosunen <marko.kosunen@aalto.fi>
2024-11-30 23:27:38 +0200

    Update

--- transferfunction_analyzer/__init__.py
+++ transferfunction_analyzer/__init__.py
@@ -1,20 +1,12 @@
 """
-=========
+=========================
 transferfunction_analyzer
-=========
+=========================
 
-transferfunction_analyzer model template The System Development Kit
-Used as a template for all TheSyDeKick Entities.
+Transferfucntion analyzer is an entity providing methods for analyzing 
+transfer functions in time and frequency domain
 
-Current docstring documentation style is Numpy
-https://numpydoc.readthedocs.io/en/latest/format.html
-
-This text here is to remind you that documentation is important.
-However, youu may find it out the even the documentation of this 
-entity may be outdated and incomplete. Regardless of that, every day 
-and in every way we are getting better and better :).
-
-Initially written by Marko Kosunen, marko.kosunen@aalto.fi, 2017.
+Initially written by Marko Kosunen, marko.kosunen@aalto.fi, 2024.
 
 """
 
@@ -24,21 +16,122 @@ if not (os.path.abspath('../../thesdk') in sys.path):
     sys.path.append(os.path.abspath('../../thesdk'))
 
 from thesdk import *
-
 import numpy as np
 
+import matplotlib.pyplot as plt
+from scipy import signal as sig
+
+import sympy as sp
+sp.init_printing()
+
+import plot_format 
+plot_format.set_style('ieeetran')
+import pdb
+
 class transferfunction_analyzer(thesdk):
+    @property
+    def poles(self):
+        if not hasattr(self,'_poles'):
+            self._poles=[]
+        return self._poles
+
+    @poles.setter
+    def poles(self,val):
+        self._poles=val
+
+
+    @property
+    def zeros(self):
+        if not hasattr(self,'_zeros'):
+            self._zeros=[]
+        return self._zeros
+
+    @zeros.setter
+    def zeros(self,val):
+        self._zeros=val
+
+    @property
+    def gain(self):
+        if not hasattr(self,'_gain'):
+            self._gain=1
+        return self._gain
+
+    @gain.setter
+    def gain(self,val):
+        self._gain=val
+
+    @property
+    def time(self):
+        if not hasattr(self,'_time'):
+            self._time=np.linspace(0,1)
+        return self._time
+
+
+    #Transer function as
+    @property
+    def tfsym(self):
+        nominator=self._g
+        self.subsdict={self._g:self.gain}
+        denominator=1
+        
+        for index in range(len(self.poles)):
+           currpolesym=sp.symbols('p%s' %(index))
+           if self.poles[index]==0:
+               denominator*=self._s
+           else:
+               denominator*=(self._s/currpolesym+1)
+           self.subsdict.update({currpolesym:self.poles[index]})
+           
+
+        for index in range(len(self.zeros)):
+           currzerosym=sp.symbols('p%s' %(index))
+           if self.zeros[index]==0:
+               nominator*=self._s
+           else:
+               nominator*=(seflf._s/currzerosym+1)
+           self.subsdict.update({currzerosym:self.zeros[index]})
+        self._tfsym=nominator/denominator
+        return self._tfsym
+
+    @property
+    def impsym(self):
+        return sp.inverse_laplace_transform(self.tfsym,self._s,self._t)
+
+    @property
+    def stepsym(self):
+        return sp.inverse_laplace_transform(1/self._s*self.tfsym,self._s,self._t)
+
+    @property
+    def tflatex(self):
+        return sp.latex(self.tfsym)
+
+    @property
+    def implatex(self):
+        return sp.latex(self.impsym)
+
+    @property
+    def steplatex(self):
+        return sp.latex(self.stepsym)
+
+    def imp(self,t):
+       imp=sp.lambdify(self._t,self.stepsym.subs(self.subsdict))
+       return np.array(imp(t))
+
+    @property
+    def impres(self):
+       self._impulse=self.ht[1].reshape(1,-1)
+       return self._stepres
+
+       self.step=self.hts[1].reshape(1,-1)
+    
 
     def __init__(self,*arg): 
         self.print_log(type='I', msg='Inititalizing %s' %(__name__)) 
-        self.proplist = [ 'Rs' ];    # Properties that can be propagated from parent
-        self.Rs =  100e6;            # Sampling frequency
-        self.IOS=Bundle()            # Pointer for input data
-        self.IOS.Members['A']=IO()   # Pointer for input data
-        self.IOS.Members['Z']= IO()
-        self.model='py';             # Can be set externally, but is not propagated
-        self.par= False              # By default, no parallel processing
-        self.queue= []               # By default, no parallel processing
+        self.model='py';
+
+        self._t=sp.symbols('t', positive=True)
+        self._s=sp.symbols('s')
+        self._g=sp.symbols('g')
 
         if len(arg)>=1:
             parent=arg[0]
@@ -50,31 +143,7 @@ class transferfunction_analyzer(thesdk):
     def init(self):
         pass #Currently nohing to add
 
-    def main(self):
-        '''Guideline. Isolate python processing to main method.
-        
-        To isolate the interna processing from IO connection assigments, 
-        The procedure to follow is
-        1) Assign input data from input to local variable
-        2) Do the processing
-        3) Assign local variable to output
 
-        '''
-        inval=self.IOS.Members['A'].Data
-        out=inval
-        if self.par:
-            self.queue.put(out)
-        self.IOS.Members['Z'].Data=out
-
-    def run(self,*arg):
-        '''Guideline: Define model depencies of executions in `run` method.
-
-        '''
-        if len(arg)>0:
-            self.par=True      #flag for parallel processing
-            self.queue=arg[0]  #multiprocessing.queue as the first argument
-        if self.model=='py':
-            self.main()
 
 if __name__=="__main__":
     import argparse
@@ -88,48 +157,4 @@ if __name__=="__main__":
     parser.add_argument('--show', dest='show', type=bool, nargs='?', const = True, 
             default=False,help='Show figures on screen')
     args=parser.parse_args()
-
-    length=1024
-    rs=100e6
-    indata=np.cos(2*math.pi/length*np.arange(length)).reshape(-1,1)
-
-    models=[ 'py']
-    duts=[]
-    plotters=[]
-    for model in models:
-        d=transferfunction_analyzer()
-        duts.append(d) 
-        d.model=model
-        d.Rs=rs
-        d.IOS.Members['A'].Data=indata
-        d.init()
-        d.run()
-
-    for k in range(len(duts)):
-        hfont = {'fontname':'Sans'}
-        figure,axes=plt.subplots(2,1,sharex=True)
-        x = np.arange(length).reshape(-1,1)
-        axes[0].plot(x,indata)
-        axes[0].set_ylim(-1.1, 1.1);
-        axes[0].set_xlim((np.amin(x), np.amax(x)));
-        axes[0].set_ylabel('Input', **hfont,fontsize=18);
-        axes[0].grid(True)
-        axes[1].plot(x, duts[k].IOS.Members['Z'].Data)
-        axes[1].set_ylim(-1.1, 1.1);
-        axes[1].set_xlim((np.amin(x), np.amax(x)));
-        axes[1].set_ylabel('Output', **hfont,fontsize=18);
-        axes[1].set_xlabel('Sample (n)', **hfont,fontsize=18);
-        axes[1].grid(True)
-        titlestr = "Myentity model %s" %(duts[k].model) 
-        plt.suptitle(titlestr,fontsize=20);
-        plt.grid(True);
-        printstr="./inv_%s.eps" %(duts[k].model)
-        plt.show(block=False);
-        figure.savefig(printstr, format='eps', dpi=300);
-    #This is here to keep the images visible
-    #For batch execution, you should comment the following line 
-    if args.show:
-       input()
-    #This is to have exit status for succesfuulexecution
-    sys.exit(0)
 
